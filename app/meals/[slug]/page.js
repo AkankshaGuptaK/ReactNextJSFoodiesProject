97c09f607e4042adf8155394e6a96c650148f756
Akanksha Gupta <akanksha.g@xitsolutionsinc.com>
2024-08-11 20:57:43 -0500

    Form data and other concepts

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,25 +1,43 @@
 import Image from "next/image";
 import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
 export default function MealsDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill />
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
-          <h1>Title</h1>
+          <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${"EMAIL"}`}>Name</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}>Summary</p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
         <p
           className={classes.instructions}
-          dangerouslySetInnerHTML={{ _html: "..." }}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
         ></p>
       </main>
     </>
